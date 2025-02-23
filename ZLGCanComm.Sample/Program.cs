96d57c0ed049680ea181b248cc6fe67760400c1b
yoiri <yoiri102@gmail.com>
2025-02-24 01:38:48 +0900

    1.2.0.0

--- ZLGCanComm.Sample/Program.cs
+++ ZLGCanComm.Sample/Program.cs
@@ -5,6 +5,8 @@ using ZLGCanComm.Records;
 
 //必须设置为X86！！！
 
+const uint _NODE_ = 0X400;//CAN节点的下行命令帧ID的前缀;
+
 /////Tcp示例
 {
     var tcpCanDevice = new TcpCanDevice("192.168.1.123", "4001");
@@ -29,8 +31,8 @@ using ZLGCanComm.Records;
     //监听设备
     tcpCanDevice.Subscribe(Received);
 
-    //发送单条帧数据
-    tcpCanDevice.Transmit(11, [1, 2, 3, 4, 5, 6, 7, 8]);
+    //发送单帧数据
+    tcpCanDevice.Transmit(_NODE_ | 11, [1, 2, 3, 4, 5, 6, 7, 8]);
 
     //发送多帧数据
     CanObject[] sendObjects = [new CanObject(), new CanObject(), new CanObject()];
@@ -57,8 +59,8 @@ using ZLGCanComm.Records;
     //监听设备
     usbCan1Device.Subscribe(Received);
 
-    //发送单条帧数据
-    usbCan1Device.Transmit(11, [1, 2, 3, 4, 5, 6, 7, 8]);
+    //发送单帧数据
+    usbCan1Device.Transmit(_NODE_ | 11, [1, 2, 3, 4, 5, 6, 7, 8]);
 
     //发送多帧数据
     CanObject[] sendObjects = [new CanObject(), new CanObject(), new CanObject()];
